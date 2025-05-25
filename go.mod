abe0bdbdb58b9eec6ba5b17e41efb9e65fcc21a0
yuchi1128 <yukiya20021128@icloud.com>
2025-05-25 21:16:06 +0900

    ymlファイルにも対応

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuchi1128/goconfig
 
 go 1.24.2
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- goconfig/config.go
+++ goconfig/config.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 func Load(filePath string, v interface{}) error {
@@ -13,11 +17,25 @@ func Load(filePath string, v interface{}) error {
 	}
 	defer file.Close()
 
-	// JSONデコーダーを作成し、ファイルの内容をデコードする
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(v); err != nil {
-		return fmt.Errorf("設定ファイル '%s' のデコードに失敗しました: %w", filePath, err)
+	ext := strings.ToLower(filepath.Ext(filePath))
+
+	switch ext {
+	case ".json":
+		// JSONデコーダーを作成し、デコードする
+		decoder := json.NewDecoder(file)
+		if err := decoder.Decode(v); err != nil {
+			return fmt.Errorf("JSONファイル '%s' のデコードに失敗しました: %w", filePath, err)
+		}
+	case ".yaml", ".yml":
+		// YAMLデコーダーを作成し、デコードする
+		decoder := yaml.NewDecoder(file)
+		if err := decoder.Decode(v); err != nil {
+			return fmt.Errorf("YAMLファイル '%s' のデコードに失敗しました: %w", filePath, err)
+		}
+	default:
+		return fmt.Errorf("サポートされていないファイル形式です: '%s' ( .json, .yaml, .yml のみサポート)", ext)
 	}
 
+	// 成功した場合は nil を返す
 	return nil
 }
